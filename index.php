bc14f47ee875792324c76e31b4571325f5483ef3
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2024-05-15 14:49:54 +0800

    g4nda ni Crush

--- index.php
+++ index.php
@@ -3,20 +3,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ticket Tamer</title>
-    
+    <link rel="stylesheet" href="library/boostrap/node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
-        <div class="col-12 col-md-4">
-            <h3 class="fw-bolder">Ticket Tamer</h3>
-            <hr>
-            <p>Username</p>
-            <input type="text" class="form-control" placeholder="Enter your Username">
-            <p>Password</p>
-            <input type="password" class="form-control" placeholder="Enter your password">
+        <div class="row">
+            <div class="col-12 col-md-4 pt-5 ">
+                <div class="p-5 border shadow rounded mt-5" style="height: 500px">
+                    <h3 class="fw-bolder mt-5">Ticket Tamer</h3>
+                    <hr>
+                    <p>Username</p>
+                    <input type="text" class="form-control" placeholder="Enter your Username">
+                    <p>Password</p>
+                    <input type="password" class="form-control" placeholder="Enter your password">
+                    <button class="btn btn-danger mt-3 w-100">Login</button>
+                </div>
+            </div>
+            <div class="col-12 col-md-8" style="padding-top: 150px; padding-left: 100px">
+                <h1 class="fw-bolder" style="font-size: 50px">Tame Your Parking Hassles!</h1>
+                <h1 class="fw-bolder" style="font-size: 50px">with Ticket Tamer</h1>
+                <img src="assets/img/robot.jpg" alt="" style="width: 500px">
+            </div>
         </div>
-        <div class="col-12 col-md-8"></div>
 
     </div>
+
+    <script src="library/jquery/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="library/boostrap/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
